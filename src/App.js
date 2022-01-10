5670e1afec44fe25cf5842e8f2d7c3cc1e78efb2
renfuwei <513837261@qq.com>
2022-01-10 14:05:12 +0800

    01-项目初始化

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import React from "react";
+import { Button } from 'antd';
+// import './App.css';
+import './App.scss';
+import TEST from './pages/test/test';
+
+
+const name = 'Jack'
+const age = 20
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="name">
+          我的名字是{name}，年龄{age}。
+        </div>
+        <div className="button-box">
+          <Button type="primary">Button</Button>
+        </div>
+        {/*测试less使用*/}
+        <div>
+          <TEST />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/test/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './test.less'
+
+function TEST() {
+  return (
+    <div className="test">
+      <span>
+        我是test，测试使用less的。
+      </span>
+    </div>
+  );
+}
+
+export default TEST;
